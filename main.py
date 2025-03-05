e0d954a265653fbf4290ea56c56a1e8c4e61644d
Jude Oluya <99790469+oluya@users.noreply.github.com>
2025-03-05 08:38:36 +0300

    Object detection and Tracking
    Started object tracking....displays in a dictionary

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from utils import read_video, save_video
+from trackers import Tracker
+
+
 def main():
     #Read Video
     video_frames = read_video("input_videos/08fd33_4.mp4")
 
+    #Initialize Tracker
+    tracker= Tracker("models/best.pt")
+
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/tracks_stubs.pkl")
     #Save Video
     save_video(video_frames, "output_videos/output_video.avi")
 

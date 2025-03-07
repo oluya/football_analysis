bed5594dd82b82d4f83578d22b00c86242127c1e
Jude Oluya <99790469+oluya@users.noreply.github.com>
2025-03-07 12:48:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def main():
     tracker= Tracker("models/best.pt")
 
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/tracks_stubs.pkl")
+
+    #Draw output
+    ## Draw Object Tracks
+    output_video_frames = tracker.draw_annotations(video_frames,tracks)
+
     #Save Video
-    save_video(video_frames, "output_videos/output_video.avi")
+    save_video(output_video_frames, "output_videos/output_video.avi")
 
 if __name__ == "__main__":
     main()

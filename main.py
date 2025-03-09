f572bc792acbc905b396b148b4be84c8d9b1be85
Jude Oluya <99790469+oluya@users.noreply.github.com>
2025-03-09 11:49:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from trackers import Tracker
 import cv2
 import numpy as np
 from team_assigner import TeamAssigner
-# from player_ball_assigner import PlayerBallAssigner
+from player_ball_assigner import PlayerBallAssigner
 # from camera_movement_estimator import CameraMovementEstimator
 # from view_transformer import ViewTransformer
 # from speed_and_distance_estimator import SpeedAndDistance_Estimator
@@ -69,19 +69,19 @@ def main():
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
 
     
-    # # Assign Ball Aquisition
-    # player_assigner =PlayerBallAssigner()
-    # team_ball_control= []
-    # for frame_num, player_track in enumerate(tracks['players']):
-    #     ball_bbox = tracks['ball'][frame_num][1]['bbox']
-    #     assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
 
-    #     if assigned_player != -1:
-    #         tracks['players'][frame_num][assigned_player]['has_ball'] = True
-    #         team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
-    #     else:
-    #         team_ball_control.append(team_ball_control[-1])
-    # team_ball_control= np.array(team_ball_control)
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
 
 
     # # Draw output 
